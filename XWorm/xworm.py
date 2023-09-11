f1674c6478634a83ff8b127551fb8605eba4ba94
cauliflowerdoughnuts <143634829+cauliflowerdoughnuts@users.noreply.github.com>
2023-09-11 12:34:20 -0400

    Update xworm.py
    changed how the config is parsed out from the binary to account for variations in config layout

--- XWorm/xworm.py
+++ XWorm/xworm.py
@@ -26,8 +26,20 @@ module = dnlib.DotNet.ModuleDefMD.Load(TARGET_PATH)
 def filter_non_printable(str):
   return ''.join([c for c in str if ord(c) > 31 or ord(c) == 9])
 
-enc_str = []
-ptext = []
+def get_key_seed(config):
+    for i in config:
+        if len(i) == 16:
+            key_seed = i
+    return bytes(key_seed, 'UTF-8')
+
+def aes_dec(enc_str, key):
+    enc_str_b64 = base64.b64decode(enc_str)
+    dec_str = malduck.aes.ecb.decrypt(key, enc_str_b64)
+    return dec_str.decode('UTF-8')
+
+
+config = []
+
 for mtype in module.GetTypes():
     if not mtype.HasMethods:
         continue
@@ -39,40 +51,29 @@ for mtype in module.GetTypes():
         if not method.Body.HasInstructions: 
             continue
         for ptr in range(len(method.Body.Instructions)):
-            if "\\Log.tmp" in method.Body.Instructions[ptr].ToString():
-                enc_str.append((method.Body.Instructions[ptr-18].Operand))
-                enc_str.append((method.Body.Instructions[ptr-16].Operand))
-                enc_str.append((method.Body.Instructions[ptr-14].Operand))
-                enc_str.append((method.Body.Instructions[ptr-12].Operand))
-                enc_str.append((method.Body.Instructions[ptr-8].Operand))
-                ptext.append((method.Body.Instructions[ptr-6].Operand))
-                key_seed = bytes(method.Body.Instructions[ptr-4].Operand, 'utf-8')
-                ptext.append((method.Body.Instructions[ptr-4].Operand))
-                ptext.append((method.Body.Instructions[ptr-2].Operand))
-                ptext.append((method.Body.Instructions[ptr].Operand))
-                enc_str.append((method.Body.Instructions[ptr+3].Operand))
-                enc_str.append((method.Body.Instructions[ptr+5].Operand))
+            if "ret" in method.Body.Instructions[ptr].ToString() and "stsfld" in method.Body.Instructions[ptr-1].ToString() and "ldstr" in method.Body.Instructions[ptr-2].ToString():
+                for ptr in range(len(method.Body.Instructions)):
+                    if method.Body.Instructions[ptr].OpCode == OpCodes.Ldstr:
+                        config.append((method.Body.Instructions[ptr].Operand))
+                    if "ldc.i4" in method.Body.Instructions[ptr].ToString(): # grab the sleep time value "IL_0028: ldc.i4.3"
+                        config.append(method.Body.Instructions[ptr].ToString()[-1])
 
-key_seed = malduck.md5(key_seed)
+
+key_seed = malduck.md5(get_key_seed(config))
 key = key_seed[0:15] + key_seed[0:16] + bytearray(b'\x00')
 
 enc_labels = ['Host', 'Port', 'AES Key', 'Splitter', 'USB Drop File', 'Telegram Token', 'Telegram Chat ID']
 enc_label_cycle = itertools.cycle(enc_labels)
 
-ptext_labels = ['Install Directory', 'Mutex/Key Seed', 'Log File Directory', 'Log File']
+ptext_labels = ['Sleep Time', 'Install Directory', 'Mutex/Key Seed', 'Log File Directory', 'Log File']
 ptext_label_cycle = itertools.cycle(ptext_labels)
 
-def aes_dec(enc_str, key):
-    enc_str_b64 = base64.b64decode(enc_str)
-    dec_str = malduck.aes.ecb.decrypt(key, enc_str_b64)
-    return dec_str.decode('UTF-8')
-
-for s in enc_str:
-    decrypted = aes_dec(s,key)
-    printable_decrypted = filter_non_printable(decrypted)
-    label = next(enc_label_cycle)
-    print(f"{label}: {printable_decrypted}")
-
-for s in ptext:
-    label = next(ptext_label_cycle)
-    print(f"{label}: {s}")
+for s in config:
+    try:
+        decrypted = aes_dec(s,key)
+        printable_decrypted = filter_non_printable(decrypted)
+        label = next(enc_label_cycle)
+        print(f"{label}: {printable_decrypted}")
+    except:
+        label = next(ptext_label_cycle)
+        print(f"{label}: {s}")

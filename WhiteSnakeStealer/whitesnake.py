4fb28504cab356e61cc08349d074dddbd3f55afa
cauliflowerdoughnuts <143634829+cauliflowerdoughnuts@users.noreply.github.com>
2024-03-28 16:51:14 +0000

    Modified config enumeration logic

--- WhiteSnakeStealer/whitesnake.py
+++ WhiteSnakeStealer/whitesnake.py
@@ -6,12 +6,12 @@ DNLIB_PATH = '/<path to>/dnlib.dll'
 clr.AddReference(DNLIB_PATH)
 from dnlib.DotNet import *
 
-LABELS = ['Log File', 'Loader URL', 'Tag', 'Mutex', 'RSA Public Key', 'Telegram Token', 'Telegram Chat ID', 'Version', 'C2', 'Install Directory', 'Data Directory', 'Transfer Hosts']
+LABELS = ['Log File', 'Loader URLs', 'Tag', 'Mutex', 'RSA Public Key', 'Telegram Token', 'Telegram Chat ID', 'Anti VM', 'Install Beacon', 'Auto Keylogger', 'Clipper Enabled',  'Version', 'Resident Method', 'Fake Message Box Icon', 'Fake Message Box Title', 'Fake Message Box Text',  'C2', 'Install Directory', 'Data Directory', 'Transfer Hosts']
 
 
 def extract_cipher_strings(file_data):
     cipher = []
-    xor = True
+    config_found = False
 
     for mtype in file_data.GetTypes():
         if not mtype.HasMethods:
@@ -21,39 +21,40 @@ def extract_cipher_strings(file_data):
                 continue
             for ptr, instruction in enumerate(method.Body.Instructions):
                 instruction_str = instruction.ToString()
+                
+                if 'System.IO.Path::GetTempPath' in instruction_str and 'ldstr' in method.Body.Instructions[ptr + 1].ToString(): # Start of config strings
+                    config_found = True
+                    continue
+                
+                if config_found and 'ldstr' in instruction_str:
+                    if instruction.Operand is None:
+                        cipher.append('Not Configured')
+                    else:
+                        cipher.append(instruction.Operand)
+                
+                if len(cipher) == 20:
+                    break
+            if len(cipher) == 20:
+                break
+        if len(cipher) == 20:
+            break
 
-                if 'ldstr "tor"' in instruction_str and 'ldstr ""' in method.Body.Instructions[ptr + 8].ToString(): # ptr+8 is the C2 config line. If it is configured it will get picked up by one of the if blocks below, otherwise it will be picked up here.
-                    cipher.insert(8, 'Not configured')
+    return cipher
 
-                if 'ldstr "<RSAKeyValue>' in instruction_str:
-                    xor = False
-                    operands_to_check = [method.Body.Instructions[ptr - 6].Operand,
-                                         method.Body.Instructions[ptr - 4].Operand,
-                                         method.Body.Instructions[ptr - 2].Operand,
-                                         method.Body.Instructions[ptr].Operand,
-                                         method.Body.Instructions[ptr + 2].Operand,
-                                         method.Body.Instructions[ptr + 4].Operand,
-                                         method.Body.Instructions[ptr + 14].Operand,
-                                         method.Body.Instructions[ptr + 24].Operand,
-                                         method.Body.Instructions[ptr + 28].Operand,
-                                         method.Body.Instructions[ptr + 33].Operand,
-                                         method.Body.Instructions[ptr + 36].Operand]
 
-                    cipher.extend(['Not configured' if not operand else operand for operand in operands_to_check])
-
-                if 'ldstr' in instruction_str and 'call' in method.Body.Instructions[ptr + 1].ToString(): # This will always match on the first config string even if not encrypted. Because of this we will not extract the first string in the if 'ldstr "<RSAKeyValue>' block above.
-                    operand = instruction.Operand
-                    cipher.append("Not configured" if not operand else operand) 
-
-    return cipher, xor
+def check_encryption(config):
+    if '<RSAKeyValue>' in config[4]:
+        return False
+    else:
+        return True
 
 
 def xor_decrypt(enc_str):
     dec_strings = []
-    text = "decr:"
+    text = 'decr:'
 
     for i in range(32, 127):
-        key = chr(i) + "sus"
+        key = chr(i) + 'sus'
         array = [chr(ord(enc_str[j]) ^ ord(key[j % len(key)])) for j in range(len(enc_str))]
         dec_string = ''.join(array)
 
@@ -66,19 +67,21 @@ def xor_decrypt(enc_str):
 def main():
     target_path = input('Enter file path to WhiteSnake payload:')
     file_data = ModuleDefMD.Load(target_path)
-    cipher, xor = extract_cipher_strings(file_data)
+    cipher = extract_cipher_strings(file_data)
+    encrypted = check_encryption(cipher)
 
-    if xor:
-        decrypted_strings = [xor_decrypt(c) if c != "Not configured" else c for c in cipher]
+    if encrypted:
+        decrypted_strings = [xor_decrypt(c) if c not in ['0', '1', 'tor'] else c for c in cipher]
+        print(f'[!] The config has been decrypted:\n')
         for label, string in zip(LABELS, decrypted_strings):
-            defanged_string = ''.join(string).replace("http", "hxxp")
-            print(f"{label}: {defanged_string}")
+            defanged_string = ''.join(string).replace('http', 'hxxp') if string else 'Not Configured'
+            print(f'{label}: {defanged_string}')
     else:
-        print(f'[!] The config was not encrypted.\n')
+        print(f'[!] The config was not encrypted:\n')
         for label, string in zip(LABELS, cipher):
-            defanged_string = ''.join(string).replace("http", "hxxp")
-            print(f"{label}: {defanged_string}")
+            defanged_string = ''.join(string).replace('http', 'hxxp') if string else 'Not Configured'
+            print(f'{label}: {defanged_string}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()

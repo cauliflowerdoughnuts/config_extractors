99ec567623f364836ae4bb593e3bf86d07a07863
cauliflowerdoughnuts <143634829+cauliflowerdoughnuts@users.noreply.github.com>
2024-11-22 11:03:53 -0500

    updated get_cipher_bytes to not break if a cipher starts with a null byte

--- Vidar/vidar_idapython_string_decrypt.py
+++ Vidar/vidar_idapython_string_decrypt.py
@@ -67,13 +67,14 @@ def find_decrypt_func():
         print("No matching sequences found.")
 
 
-def get_cipher_bytes(start_address):
+def get_cipher_bytes(start_address, max_length=1000):
     result = []
-    current_byte = idc.get_wide_byte(start_address)
-    while current_byte != 0 and len(result) < 1000:
+    for _ in range(max_length):
+        current_byte = idc.get_wide_byte(start_address)
         result.append(current_byte)
         start_address += 1
-        current_byte = idc.get_wide_byte(start_address)
+        if current_byte == 0 and len(result) > 1:
+            break
     return result
 
 
@@ -85,6 +86,7 @@ def xor_decrypt(cipher, key):
     for i in range(len(cipher)):
         decrypted_text[i] = cipher[i] ^ key[i % len(key)]
     return bytes(decrypted_text).decode(errors='ignore')
+
 
 def find_xrefs_and_decrypt(decrypt_func_address):
     for ref in XrefsTo(decrypt_func_address):
@@ -102,7 +104,7 @@ def find_xrefs_and_decrypt(decrypt_func_address):
         cipher_address = idc.get_operand_value(push_cipher_address, 0)
         key_address = idc.get_operand_value(push_key_address, 0)
         
-        cipher = get_cipher_bytes(cipher_address)
+        cipher = get_cipher_bytes(cipher_address) # for some reason idaapi.get_arg_addrs wont extract the cipher length argument so we cant use the built in get_bytes function
         cipher_len = len(cipher)
         key = get_bytes(key_address, cipher_len)
         
